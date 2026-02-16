81ca9d30530313e6aead241c8f011011750ffc99
kevingadelha <45942920+kevingadelha@users.noreply.github.com>
2026-02-16 13:25:24 -0500

    Hide borders

--- Form1.Designer.cs
+++ Form1.Designer.cs
@@ -46,8 +46,7 @@
             // 
             // label1
             // 
-            label1.BackColor = Color.White;
-            label1.BorderStyle = BorderStyle.FixedSingle;
+            label1.BackColor = Color.Orange;
             label1.Dock = DockStyle.Fill;
             label1.Font = new Font("Segoe UI", 150F);
             label1.Location = new Point(3, 0);
@@ -59,124 +58,124 @@
             // 
             // label2
             // 
-            label2.BackColor = Color.White;
-            label2.BorderStyle = BorderStyle.FixedSingle;
+            label2.BackColor = Color.Orange;
             label2.Dock = DockStyle.Fill;
             label2.Font = new Font("Segoe UI", 24F);
-            label2.Location = new Point(3, 0);
+            label2.Location = new Point(0, 0);
+            label2.Margin = new Padding(0);
             label2.Name = "label2";
-            label2.Size = new Size(252, 332);
+            label2.Size = new Size(260, 334);
             label2.TabIndex = 1;
             label2.Text = "label2";
             label2.TextAlign = ContentAlignment.MiddleCenter;
             // 
             // label3
             // 
-            label3.BackColor = Color.White;
-            label3.BorderStyle = BorderStyle.FixedSingle;
+            label3.BackColor = Color.Orange;
             label3.Dock = DockStyle.Fill;
             label3.Font = new Font("Segoe UI", 24F);
-            label3.Location = new Point(261, 0);
+            label3.Location = new Point(260, 0);
+            label3.Margin = new Padding(0);
             label3.Name = "label3";
-            label3.Size = new Size(252, 332);
+            label3.Size = new Size(260, 334);
             label3.TabIndex = 2;
             label3.Text = "label3";
             label3.TextAlign = ContentAlignment.MiddleCenter;
             // 
             // label4
             // 
-            label4.BackColor = Color.White;
-            label4.BorderStyle = BorderStyle.FixedSingle;
+            label4.BackColor = Color.Orange;
             label4.Dock = DockStyle.Fill;
             label4.Font = new Font("Segoe UI", 24F);
-            label4.Location = new Point(519, 0);
+            label4.Location = new Point(520, 0);
+            label4.Margin = new Padding(0);
             label4.Name = "label4";
-            label4.Size = new Size(254, 332);
+            label4.Size = new Size(262, 334);
             label4.TabIndex = 3;
             label4.Text = "label4";
             label4.TextAlign = ContentAlignment.MiddleCenter;
             // 
             // label5
             // 
-            label5.BackColor = Color.White;
-            label5.BorderStyle = BorderStyle.FixedSingle;
+            label5.BackColor = Color.Orange;
             label5.Dock = DockStyle.Fill;
             label5.Font = new Font("Segoe UI", 24F);
-            label5.Location = new Point(3, 332);
+            label5.Location = new Point(0, 334);
+            label5.Margin = new Padding(0);
             label5.Name = "label5";
-            label5.Size = new Size(252, 332);
+            label5.Size = new Size(260, 334);
             label5.TabIndex = 4;
             label5.Text = "label5";
             label5.TextAlign = ContentAlignment.MiddleCenter;
             // 
             // label6
             // 
-            label6.BackColor = Color.White;
-            label6.BorderStyle = BorderStyle.FixedSingle;
+            label6.BackColor = Color.Orange;
             label6.Dock = DockStyle.Fill;
             label6.Font = new Font("Segoe UI", 24F);
-            label6.Location = new Point(261, 332);
+            label6.Location = new Point(260, 334);
+            label6.Margin = new Padding(0);
             label6.Name = "label6";
-            label6.Size = new Size(252, 332);
+            label6.Size = new Size(260, 334);
             label6.TabIndex = 5;
             label6.Text = "label6";
             label6.TextAlign = ContentAlignment.MiddleCenter;
             // 
             // label7
             // 
-            label7.BackColor = Color.White;
-            label7.BorderStyle = BorderStyle.FixedSingle;
+            label7.BackColor = Color.Orange;
             label7.Dock = DockStyle.Fill;
             label7.Font = new Font("Segoe UI", 24F);
-            label7.Location = new Point(519, 332);
+            label7.Location = new Point(520, 334);
+            label7.Margin = new Padding(0);
             label7.Name = "label7";
-            label7.Size = new Size(254, 332);
+            label7.Size = new Size(262, 334);
             label7.TabIndex = 6;
             label7.Text = "label7";
             label7.TextAlign = ContentAlignment.MiddleCenter;
             // 
             // label8
             // 
-            label8.BackColor = Color.White;
-            label8.BorderStyle = BorderStyle.FixedSingle;
+            label8.BackColor = Color.Orange;
             label8.Dock = DockStyle.Fill;
             label8.Font = new Font("Segoe UI", 24F);
-            label8.Location = new Point(3, 664);
+            label8.Location = new Point(0, 668);
+            label8.Margin = new Padding(0);
             label8.Name = "label8";
-            label8.Size = new Size(252, 332);
+            label8.Size = new Size(260, 334);
             label8.TabIndex = 7;
             label8.Text = "label8";
             label8.TextAlign = ContentAlignment.MiddleCenter;
             // 
             // label9
             // 
-            label9.BackColor = Color.White;
-            label9.BorderStyle = BorderStyle.FixedSingle;
+            label9.BackColor = Color.Orange;
             label9.Dock = DockStyle.Fill;
             label9.Font = new Font("Segoe UI", 24F);
-            label9.Location = new Point(261, 664);
+            label9.Location = new Point(260, 668);
+            label9.Margin = new Padding(0);
             label9.Name = "label9";
-            label9.Size = new Size(252, 332);
+            label9.Size = new Size(260, 334);
             label9.TabIndex = 8;
             label9.Text = "label9";
             label9.TextAlign = ContentAlignment.MiddleCenter;
             // 
             // label10
             // 
-            label10.BackColor = Color.White;
-            label10.BorderStyle = BorderStyle.FixedSingle;
+            label10.BackColor = Color.Orange;
             label10.Dock = DockStyle.Fill;
             label10.Font = new Font("Segoe UI", 24F);
-            label10.Location = new Point(519, 664);
+            label10.Location = new Point(520, 668);
+            label10.Margin = new Padding(0);
             label10.Name = "label10";
-            label10.Size = new Size(254, 332);
+            label10.Size = new Size(262, 334);
             label10.TabIndex = 9;
             label10.Text = "label10";
             label10.TextAlign = ContentAlignment.MiddleCenter;
             // 
             // tableLayoutPanel1
             // 
-            tableLayoutPanel1.BackColor = Color.White;
+            tableLayoutPanel1.BackColor = Color.Orange;
             tableLayoutPanel1.ColumnCount = 2;
             tableLayoutPanel1.ColumnStyles.Add(new ColumnStyle(SizeType.Percent, 50F));
             tableLayoutPanel1.ColumnStyles.Add(new ColumnStyle(SizeType.Percent, 50F));
@@ -184,6 +183,7 @@
             tableLayoutPanel1.Controls.Add(label1, 0, 0);
             tableLayoutPanel1.Dock = DockStyle.Fill;
             tableLayoutPanel1.Location = new Point(0, 0);
+            tableLayoutPanel1.Margin = new Padding(0);
             tableLayoutPanel1.Name = "tableLayoutPanel1";
             tableLayoutPanel1.RowCount = 1;
             tableLayoutPanel1.RowStyles.Add(new RowStyle(SizeType.Percent, 50F));
@@ -192,7 +192,7 @@
             // 
             // tableLayoutPanel2
             // 
-            tableLayoutPanel2.BackColor = Color.White;
+            tableLayoutPanel2.BackColor = Color.Orange;
             tableLayoutPanel2.ColumnCount = 3;
             tableLayoutPanel2.ColumnStyles.Add(new ColumnStyle(SizeType.Percent, 33.3333321F));
             tableLayoutPanel2.ColumnStyles.Add(new ColumnStyle(SizeType.Percent, 33.3333321F));
@@ -207,13 +207,14 @@
             tableLayoutPanel2.Controls.Add(label7, 2, 1);
             tableLayoutPanel2.Controls.Add(label6, 1, 1);
             tableLayoutPanel2.Dock = DockStyle.Fill;
-            tableLayoutPanel2.Location = new Point(784, 3);
+            tableLayoutPanel2.Location = new Point(781, 0);
+            tableLayoutPanel2.Margin = new Padding(0);
             tableLayoutPanel2.Name = "tableLayoutPanel2";
             tableLayoutPanel2.RowCount = 3;
             tableLayoutPanel2.RowStyles.Add(new RowStyle(SizeType.Percent, 33.3333321F));
             tableLayoutPanel2.RowStyles.Add(new RowStyle(SizeType.Percent, 33.3333321F));
             tableLayoutPanel2.RowStyles.Add(new RowStyle(SizeType.Percent, 33.3333321F));
-            tableLayoutPanel2.Size = new Size(776, 996);
+            tableLayoutPanel2.Size = new Size(782, 1002);
             tableLayoutPanel2.TabIndex = 0;
             // 
             // Form1
